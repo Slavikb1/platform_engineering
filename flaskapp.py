035efabbb7a9e36be9de675b3b0f88cd54a8825b
slavik.bidarski <slavikb1@yahoo.com>
2024-06-17 10:58:09 +0300

    adding files

--- flaskapp.py
+++ flaskapp.py
@@ -7,49 +7,30 @@ from datetime import datetime
 from flask import Flask, render_template, request, redirect, send_from_directory
 from dotenv import load_dotenv
 from prometheus_flask_exporter import PrometheusMetrics
-from prometheus_client import Counter, generate_latest
+from prometheus_client import Counter
+from pymongo import MongoClient
 
-
-
-load_dotenv()  # load data from .env file
+load_dotenv()  # load data from.env file
 api_key = os.getenv("APIKey")  # grab the key from the file
 
-# initialize flask app, create object of class Flask
+# Initialize Flask app
 app = Flask(__name__)
 metrics = PrometheusMetrics(app)
 metrics.info("app_info", "App Info, this can be anything you want", version="1.0.0")
 
-
-def data_history(city_name):
-    data = {'Date': datetime.now().strftime("%d-%m-%Y %H:%M:%S"), 'City': city_name}
-    path = './history/data.json'
-    if not os.path.exists(path):
-        with open(path, 'a') as f:
-            json.dump([data], f)
-    elif not os.path.getsize(path) > 0:
-        with open(path, 'a') as f:
-            json.dump([data], f)
-    else:
-        with open(path, 'r') as f:
-            temp = json.load(f)
-            temp.append(data)
-        with open(path, 'w') as f:
-            json.dump(temp, f)
-
+# MongoDB Setup
+mongo_uri = "mongodb://localhost:27017"  # Change this to your MongoDB URI
+client = MongoClient(mongo_uri)
+db = client['weather_app_db']  # Database name
+collection = db['weather_data']  # Collection name
 
 # Configure Flask logging
 app.logger.setLevel(logging.INFO)  # Set log level to INFO
 handler = logging.FileHandler('app.log')  # Log to a file
 app.logger.addHandler(handler)
 
-#view_metric = Counter('view', 'Product view', ['product'])
-#buy_metric = Counter('buy', 'Product buy', ['product'])
-
-city_metric = Counter('city', 'Cities view', ['city_name'])
-
 AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
-
 
 @app.route("/", methods=['POST', 'GET'])  # GET is used to request data from a specified resource
 def index():  # POST is used to send data to a server to create/update a resource.
@@ -68,21 +49,20 @@ def index():  # POST is used to send data to a server to create/update a resourc
         return render_template("index.html", weather=None, error=1)
 
     weather = {
-        'day': final_weather_data[0],  # 7
-        'week': final_weather_data[3],  # 7
-        'night': final_weather_data[1],  # 7
-        'humidity': final_weather_data[2],  # 7
-        'country': lon_lat_country[2],  # 1
-        'city': city,  # 1
-        'date': final_weather_data[4]  # 7
+        'day': final_weather_data[0],
+        'week': final_weather_data[3],
+        'night': final_weather_data[1],
+        'humidity': final_weather_data[2],
+        'country': lon_lat_country[2],
+        'city': city,
+        'date': final_weather_data[4]
     }
-    
-#    city_metric = Counter('city', 'Cities view', ['city_name'])
 
     app.logger.info('searched for: ' + city)
     city_metric.labels(city_name=city).inc()
-    
-    data_history(city)
+
+    # Save weather data to MongoDB
+    collection.insert_one(weather)
 
     return render_template("index.html", weather=weather)
 
@@ -107,7 +87,6 @@ def telaviv():
     if final_weather_data == 0:
         return render_template("index.html", weather=None, error=1)
 
-#    weather_data = weather(city)
     dynamodb_client = boto3.client('dynamodb', aws_access_key_id=AWS_ACCESS_KEY_ID,
                                    aws_secret_access_key=AWS_SECRET_ACCESS_KEY, region_name='us-east-1')
     dynamodb_client.put_item(
@@ -115,20 +94,9 @@ def telaviv():
     )
     return render_template('index.html')
 
-#@app.route('/view/<id>')
-#def view_product(id):
-#    view_metric.labels(product=id).inc()
-#    return "View %s" % id
-
-#@app.route('/buy/<id>')
-#def buy_product(id):
-#    buy_metric.labels(product=id).inc()
-#    return "Buy %s" % id
-
 @app.route('/metrics')
 def metrics():
     return generate_latest()
-
 
 @app.errorhandler(500)
 def server_error(error):
