3bb9908a6e7533e9d566a55a83e362be7b80fdd7
slavik.bidarski <slavikb1@yahoo.com>
2024-06-18 12:58:28 +0300

    check

--- flaskapp.py
+++ flaskapp.py
@@ -3,6 +3,7 @@ import boto3
 import json
 import API_data
 from datetime import datetime
+from pymongo import MongoClient
 from flask import Flask, render_template, request, redirect, send_from_directory
 from dotenv import load_dotenv
 
@@ -12,11 +13,17 @@ api_key = os.getenv("APIKey")  # grab the key from the file
 # initialize flask app, create object of class Flask
 app = Flask(__name__)
 
+# MongoDB configuration
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+db = client.weather_db  # Use the appropriate database name
+searches_collection = db.searches  # Collection to store searches
 
 @app.route("/", methods=['POST', 'GET'])  # GET is used to request data from a specified resource
 def index():  # POST is used to send data to a server to create/update a resource.
     if request.method == 'GET':
         return render_template("index.html", weather="")
+
     city = request.form['Search']
 
     lon_lat_country = API_data.lat_lon(city, api_key)
@@ -38,6 +45,14 @@ def index():  # POST is used to send data to a server to create/update a resourc
         'city': city,  # 1
         'date': final_weather_data[4]  # 7
     }
+
+    # Save the search to MongoDB
+    search_entry = {
+        'city': city,
+        'country': lon_lat_country[2],
+        'search_time': datetime.now().isoformat()
+    }
+    searches_collection.insert_one(search_entry)
 
     return render_template("index.html", weather=weather)
 
